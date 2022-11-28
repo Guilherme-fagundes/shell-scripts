ae9c31d6c883b868531cc1a1b6d30d3a209a2b30
Guilherme-fagundes <guilhermekfagundes259@gmail.com>
2022-11-27 23:50:52 -0300

    upload git-zsh-curl installer shell

---
+++ installer-git-szh-curl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Created by: Guilherme K. Fagundes
+#Date: 2022-11-27
+#Description: This shell script is to installer git, zsh terminal and curl.
+
+sudo apt update
+
+#Installer softwares commands
+sudo apt install git zsh curl -y
